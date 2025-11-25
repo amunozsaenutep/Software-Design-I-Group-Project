140abc989c8dd7efb6be6b7e08a69970488618c6
amunozsaenutep <amunozsaen@miners.utep.edu>
2025-11-25 00:55:08 -0700

    Update mainFunctions.h

--- mainFunctions.h
+++ mainFunctions.h
@@ -21,27 +21,33 @@ typedef struct //List of accounts structure
 //Create a list
 accLIST clientsLIST;
 
+//Create a file
+FILE *clientsFILES;
+
 void add_client_info(accLIST *clientsLIST)
 {
 	accINFO *newClient = malloc(sizeof(accINFO));
 
-	// Poll the user for the coordinates of the new point
-	printf("Enter the new point's coordinates\n");
-	//printf("x: "); scanf("%lg", &(newClient->x));
-	//printf("y: "); scanf("%lg", &(newClient->y));
+	// Poll the user for the information of the new client
+	printf("Enter the new client's information\n");
+	printf("First Name: "); scanf("%s", newClient->firstName);
+	printf("Last Name: "); scanf("%s", newClient->lastName);
+	printf("Account ID: "); scanf("%d", &(newClient->accountID));
+	printf("Initial Balance: "); scanf("%lg", &(newClient->accountBalance));
+
 	newClient->next = NULL; newClient->prev = NULL;	// default pointer values are NULL
 
-	// Queue the point at the end of the existing shape's point list
+	// Queue the client at the end of the existing list
 	if(clientsLIST->tail == NULL)
 	{
-		// point list was empty
+		// clients list was empty
 		clientsLIST->count = 1;
 		clientsLIST->head = newClient;
 		clientsLIST->tail = newClient;
 	}
 	else
 	{
-		// Add new point to the end of the point list
+		// Add new client to the end of the list
 		clientsLIST->count++;
 		clientsLIST->tail->next = newClient;
 		newClient->prev = clientsLIST->tail;
@@ -54,22 +60,44 @@ void print_clients_list(accLIST *clientsLIST)
 {
 	accINFO *travPtr;
 
-	// If the shape has no points, print a message
+	// If the list has no clients, print a message
 	if(clientsLIST->head == NULL)
 	{
-		printf("Shape undefined\n");
+		printf("List undefined\n");
 		return;
 	}
 
-	// Traverse the shape's list of points and print their coordinates
+	// Traverse the list of clients and print their info
 	travPtr = clientsLIST->head;
 	while(travPtr != NULL)
 	{
-		//printf("x = %lg, y = %lg\n", travPtr->x, travPtr->y);
+		printf("First Name: %s\n Last Name: %s\n Account ID: %d\n Account Balance: %lg\n\n", travPtr->firstName, travPtr->lastName,travPtr->accountID,travPtr->accountBalance);
 		travPtr = travPtr->next;
 	}
 
 	return;
+}
+
+void store_list(accLIST *clientsLIST)
+{
+	clientsFILES = fopen("clients.bin","wb");
+
+	if(clientsFILES == NULL)
+	{
+		perror("Error opening file\n");
+	}
+
+	size_t written_records = fwrite(clientsLIST,sizeof(clientsLIST), clientsLIST->count,clientsFILES);
+
+	if (written_records != clientsLIST->count)
+	{
+		perror("Error writing to file\n");
+	}
+	else
+	{
+		printf("%zu elements successfully written\n", written_records);
+	}
+	fclose(clientsFILES);
 }
 /*  void store_list(clientsLIST *theList)  PENDING FUNCTIONS
 	void load_list(clientsLIST *theList)
